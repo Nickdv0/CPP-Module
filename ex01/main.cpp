415a41ea75f3976d84156d97844279724069e468
nde-vant <nicolas.devantery@gmail.com>
2024-09-16 17:01:51 +0200

    refactor: Refactor main.cpp and main.hpp
    Refactor the main.cpp and main.hpp files to improve code structure and user interaction. This includes:
    - Adding a loop to continuously prompt the user for commands
    - Implementing the "ADD" and "SEARCH" commands
    - Handling invalid commands with an appropriate error message
    
    These changes aim to enhance the functionality and user experience of the program.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,9 +6,28 @@
 /*   By: nde-vant <nde-vant@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 14:29:31 by nde-vant          #+#    #+#             */
-/*   Updated: 2024/09/12 14:36:40 by nde-vant         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:50:06 by nde-vant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "main.hpp"
 
-
+int	main(void)
+{
+	std::string input;
+	while (true)
+	{
+		std::cout << "Enter one of the following commands :\n\tADD\n\tSEARCH\n\tEXIT\n";
+		std::cin >> input;
+		if (std::string("ADD").compare(input))
+			continue ;
+		else if (std::string("SEARCH").compare(input))
+			continue ;
+		else if (std::string("EXIT").compare(input))
+			break ;
+		else
+			std::cout << "Invalid command. Please try again.\n";
+	}
+	std::cout << "Exiting program.\n";
+	return (0);
+}
--- ex01/main.hpp
+++ ex01/main.hpp
@@ -6,7 +6,7 @@
 /*   By: nde-vant <nde-vant@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 14:36:32 by nde-vant          #+#    #+#             */
-/*   Updated: 2024/09/12 14:38:54 by nde-vant         ###   ########.fr       */
+/*   Updated: 2024/09/16 17:01:31 by nde-vant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 #define MAIN_HPP
 
 #include <iostream>
+#include <string>
+
+class PhoneBook {
+	public:
+		int		index = 0;
+		Contact contacts[8];
+	
+	void	AddContact(std::string FirstName, std::string LastName, std::string	NickName, std::string PhoneNumber, std::string DarkestSecret)
+	{
+		
+	}
+
+	void	SearchContact(std::string KeyWord)
+	{
+		
+	}
+};
+
+class Contact {
+	private:
+		std::string DarkestSecret[11];
+	public:
+		std::string FirstName[11];
+		std::string LastName[11];
+		std::string	NickName[11];
+		std::string	PhoneNumber[11];
+};
 
 #endif
