94ed6c1b0e1a65f93025f3156db82a6d35642a8a
NickDV0 <nicolas.devantery@gmail.com>
2024-11-19 14:33:43 +0100

    Refactor variable names in main.cpp for improved clarity and readability

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nde-vant <nde-vant@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:13:55 by nde-vant          #+#    #+#             */
-/*   Updated: 2024/11/19 14:32:46 by nde-vant         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:33:36 by nde-vant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 int		main(void)
 {
 	std::string		str = "HI THIS IS BRAIN";
-	std::string*	pointer = &str;
-	std::string&	ref = str;
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
 
-	std::cout << "Pointer address: " << pointer << std::endl;
-	std::cout << "Reference address: " << &ref << std::endl;
+	std::cout << "Pointer address: " << stringPTR << std::endl;
+	std::cout << "Reference address: " << &stringREF << std::endl;
 
-	std::cout << "Pointer value: " << *pointer << std::endl;
-	std::cout << "Reference value: " << ref << std::endl;
+	std::cout << "Pointer value: " << *stringPTR << std::endl;
+	std::cout << "Reference value: " << stringREF << std::endl;
 
 	// *pointer = "HI THIS IS NEW BRAIN";
 
