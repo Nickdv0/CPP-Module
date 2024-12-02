470247536eaed67cdcc8cf558a5413317a3d7da4
NickDV0 <nicolas.devantery@gmail.com>
2024-12-02 16:41:31 +0100

    Add Harl class implementation and Makefile for project setup

---
+++ 01/ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-vant <nde-vant@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 13:25:36 by nde-vant          #+#    #+#             */
+/*   Updated: 2024/12/02 16:36:11 by nde-vant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl {
+	private:
+		void debug(void) const;
+		void info(void) const;
+		void warning(void) const;
+		void error(void) const;
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain(const int level) const;
+};
+
+#endif
